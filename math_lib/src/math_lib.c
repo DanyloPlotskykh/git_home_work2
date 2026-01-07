58c50465ea1729d4fda265e5492d8dd5b6cb285e
DanyloPlotskykh <145497398+DanyloPlotskykh@users.noreply.github.com>
2026-01-08 00:25:23 +0200

    Add 2nd function

--- math_lib/src/math_lib.c
+++ math_lib/src/math_lib.c
@@ -4,3 +4,8 @@ uint32_t MATH_add(uint32_t a, uint32_t b)
 {
     return (UINT32_MAX - a < b) ? UINT32_MAX : (a + b);
 }
+
+uint32_t MATH_sub(uint32_t a, uint32_t b)
+{
+    return (b > a) ? 0 : (a - b);
+}
