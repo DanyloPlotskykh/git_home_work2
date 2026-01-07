372f7fd37dd92c176efc11879b7ebaa9a0e7cc59
DanyloPlotskykh <145497398+DanyloPlotskykh@users.noreply.github.com>
2026-01-08 00:27:44 +0200

    Add 1st function

---
+++ math_lib/inc/math_lib.h
@@ -0,0 +1,8 @@
+#ifndef MATH_h
+#define MATH_h
+
+#include <stdint.h>
+
+uint32_t MATH_mul(uint32_t a, uint32_t b);
+
+#endif
---
+++ math_lib/src/math_lib.c
@@ -0,0 +1,11 @@
+#include "math_lib.h"
+
+uint32_t MATH_mul(uint32_t a, uint32_t b)
+{
+    if (a != 0 && b > UINT32_MAX / a)
+    {
+        return UINT32_MAX;
+    }
+
+    return a * b;
+}
